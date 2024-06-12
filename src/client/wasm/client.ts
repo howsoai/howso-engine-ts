a2107c98a4bfa9321cbb9cf2705588e4f7f7064d
Lance Gliser <lance.gliser@howso.com>
2024-06-12 09:50:58 -0500

    20298: Remove default action and feature contexts (#7)

--- src/client/wasm/client.ts
+++ src/client/wasm/client.ts
@@ -262,8 +262,6 @@ export class WasmClient extends BaseClient implements ITraineeClient, ISessionCl
       name: metadata.name,
       persistence: metadata.persistence,
       metadata: metadata.metadata,
-      default_action_features: metadata.default_action_features || [],
-      default_context_features: metadata.default_context_features || [],
     }) as Trainee;
   }
 
@@ -458,8 +456,6 @@ export class WasmClient extends BaseClient implements ITraineeClient, ISessionCl
     const metadata = {
       name: props.name,
       metadata: props.metadata,
-      default_context_features: props.default_context_features,
-      default_action_features: props.default_action_features,
       persistence: props.persistence || "allow",
     };
     await this.execute(traineeId, "set_metadata", { metadata });
@@ -874,13 +870,11 @@ export class WasmClient extends BaseClient implements ITraineeClient, ISessionCl
    * @param request The react request.
    */
   private preprocessReactRequest(trainee: Trainee, request: ReactRequest | ReactSeriesRequest): void {
-    if (request.desired_conviction == null) {
-      if (request.action_features == null) {
-        request.action_features = trainee.default_action_features;
-      }
-      if (request.context_features == null) {
-        request.context_features = trainee.default_context_features;
-      }
+    if (!trainee) {
+      throw new Error("trainee is undefined");
+    }
+    if (!request) {
+      throw new Error("request is undefined");
     }
   }
 }
