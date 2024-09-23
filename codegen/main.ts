45da164356df68adde2b01ae2ed2abc3163d5558
Matt Fulp <8397318+fulpm@users.noreply.github.com>
2024-09-23 15:56:44 -0400

    docs

--- codegen/main.ts
+++ codegen/main.ts
@@ -2,6 +2,7 @@ import { getEngineApi } from "./engine";
 import { Generator } from "./generator";
 
 export default async function main() {
+  // Entrypoint for running the code generator
   const doc = await getEngineApi();
   const gen = new Generator(doc);
   gen.render();
