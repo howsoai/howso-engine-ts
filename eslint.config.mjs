2377ec062840e137969539b1b8ab2d1223921f69
Lance Gliser <lance.gliser@howso.com>
2024-09-24 17:06:20 -0500

    @types/jest@^29.5.13

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,15 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
       "@typescript-eslint/no-empty-object-type": "warn",
       "@typescript-eslint/no-explicit-any": "off",
