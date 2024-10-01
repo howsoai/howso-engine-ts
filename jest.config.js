3ef055c1281123052e9c525807d87944fd717693
Lance Gliser <lance.gliser@howso.com>
2024-10-01 14:49:16 -0500

    Added a jest config to allow the use of d3-dsv

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^d3-dsv$": "<rootDir>/node_modules/d3-dsv/dist/d3-dsv.min.js",
+  },
   testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
   transform: {
     "^.+\\.[tj]s$": [
